7144be6aa4c87e0cbc013711a74b3f6952490231
Patrick Morais <ppternunes@gmail.com>
2022-08-27 15:08:31 -0300

    fix(App): remove component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
-import { Hello } from './components/Hello'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 import { Router } from './routes'
@@ -11,7 +10,6 @@ function App() {
       <ThemeProvider theme={defaultTheme}>
         <Router />
         <GlobalStyle />
-        <Hello />
       </ThemeProvider>
     </BrowserRouter>
   )
--- src/components/Hello.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export function Hello() {
-  return <div data-testid="hello">Hello World</div>
-}
