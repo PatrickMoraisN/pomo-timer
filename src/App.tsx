04b8142a6b373f2ccc4121d88885f0ae5330f2b3
Patrick Morais <ppternunes@gmail.com>
2022-08-27 14:49:35 -0300

    feat(App): add BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 import { Hello } from './components/Hello'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { Router } from './routes'
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <Hello />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyle />
+        <Hello />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
 
