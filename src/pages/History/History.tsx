f5560fe130e17ebad9041cb4246a44e0944abb93
Patrick Morais <ppternunes@gmail.com>
2022-08-30 10:03:15 -0300

    feat(History): create static page

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,5 +1,49 @@
 import React from 'react'
+import * as S from './History.styles'
 
 export function History() {
-  return <h1>History</h1>
+  return (
+    <S.HistoryContainer>
+      <h1>My History</h1>
+
+      <S.HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluido</td>
+            </tr>
+          </tbody>
+        </table>
+      </S.HistoryList>
+    </S.HistoryContainer>
+  )
 }
