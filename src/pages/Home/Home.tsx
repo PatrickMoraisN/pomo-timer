be7d6b54bc67cb487abd90e1a5ff8f384d84604e
Patrick Morais <ppternunes@gmail.com>
2022-08-30 21:09:16 -0300

    feat(Home): add react-hook-form on form

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,39 @@
 import { Play } from 'phosphor-react'
 import * as S from './Home.styles'
+import { useForm } from 'react-hook-form'
 import React from 'react'
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm()
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <S.HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <S.FormContainer>
           <label htmlFor="task">I will work in</label>
-          <S.TaskInput type="text" id="task" placeholder="name your project" />
+          <S.TaskInput
+            type="text"
+            id="task"
+            placeholder="name your project"
+            {...register('task')}
+          />
 
           <label htmlFor="minutesAmount">for</label>
           <S.MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutes.</span>
@@ -28,7 +47,7 @@ export function Home() {
           <span>0</span>
         </S.CountdownContainer>
 
-        <S.StartCountdownButton type="submit">
+        <S.StartCountdownButton type="submit" disabled={!isSubmitDisabled}>
           <Play />
           START
         </S.StartCountdownButton>
