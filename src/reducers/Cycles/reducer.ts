f6a89d9de7b49e6ee9d04039d378ce7de2141309
Patrick Morais <ppternunes@gmail.com>
2022-10-01 14:43:33 -0300

    feat(CyclesReducer - reducer): create reducer

---
+++ src/reducers/Cycles/reducer.ts
@@ -0,0 +1,42 @@
+import { Cycle } from '../../contexts/CyclesContext'
+import { ActionTypes } from './actions'
+
+interface CyclesStateProps {
+  cycles: Cycle[]
+  activeCycleId: string | null
+}
+
+export function cyclesReducer(state: CyclesStateProps, action: any) {
+  switch (action.type) {
+    case ActionTypes.ADD_NEW_CYCLE:
+      return {
+        ...state,
+        cycles: [...state.cycles, action.payload.newCycle],
+        activeCycleId: action.payload.newCycle.id,
+      }
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE:
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, interruptedDate: new Date() }
+          }
+          return cycle
+        }),
+        activeCycleId: null,
+      }
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, finishedDate: new Date() }
+          }
+          return cycle
+        }),
+      }
+
+    default:
+      return state
+  }
+}
