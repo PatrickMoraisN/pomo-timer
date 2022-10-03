499e5aa0464e5b06286b547779a4a889a2607d63
Patrick Morais <ppternunes@gmail.com>
2022-10-02 21:34:43 -0300

    delete(Helllo): delete file

--- src/Hello.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import { Hello } from './components/Hello'
-
-describe('Hello Wolrd', () => {
-  it('Hello', () => {
-    const { getByText } = render(<Hello />)
-    expect(getByText('Hello World')).toBeInTheDocument()
-  })
-})
