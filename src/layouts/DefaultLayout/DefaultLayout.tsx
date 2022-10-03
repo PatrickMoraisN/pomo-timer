4fa3e39a6bf85facc79aff268b5c1e3a2a3f30ab
Patrick Morais <ppternunes@gmail.com>
2022-10-03 20:50:33 -0300

    refactor(DefaultLayout): add custom paths

--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,5 +1,5 @@
+import { Header } from '@components/Header'
 import { Outlet } from 'react-router-dom'
-import { Header } from '../../components'
 import * as Styles from './DefaultLayout.styles'
 
 export function DefaultLayout() {
