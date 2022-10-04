45681d9cb0686f033fcb436809bcc2ebe518b628
Patrick Morais <ppternunes@gmail.com>
2022-10-04 10:23:59 -0300

    refactor(App): change BrowserRouter to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from 'styled-components'
-import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 import { Router } from './routes'
@@ -8,16 +7,14 @@ import { CountdownContextProvider } from '@context/CountdownContext'
 
 function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={defaultTheme}>
-        <CyclesContextProvider>
-          <CountdownContextProvider>
-            <Router />
-          </CountdownContextProvider>
-        </CyclesContextProvider>
-        <GlobalStyle />
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <CyclesContextProvider>
+        <CountdownContextProvider>
+          <Router />
+        </CountdownContextProvider>
+      </CyclesContextProvider>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
