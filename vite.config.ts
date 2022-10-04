846f28a24bab7867a6dcd465489600e478ca3d41
Patrick Morais <ppternunes@gmail.com>
2022-10-03 21:05:56 -0300

    chore(vite.config): add alias imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
   plugins: [svgr(), react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@test': path.resolve(__dirname, './src/test'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@enums': path.resolve(__dirname, './src/enums'),
+      '@context': path.resolve(__dirname, './src/contexts'),
+      '@reducers': path.resolve(__dirname, './src/reducers'),
+    },
+  },
 })
